47746c0dcae516ca2a323818bcf65b7fcf55cb14
iprateek13 <165635700+iprateek13@users.noreply.github.com>
2025-07-20 12:13:26 +0530

    Update db.js

--- CONNECTIONWITHMONGO/db.js
+++ CONNECTIONWITHMONGO/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log(`mongodb+srv://prateek13:prateek233001@clusterone.f6krgcf.mongodb.net`);
+  
     const conn = await mongoose.connect(process.env.Mongodb_uri);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
